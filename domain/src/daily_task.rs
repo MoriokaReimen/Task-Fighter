7d0f9a63d43f77939b4b9c7660c56427786aaca1
MoriokaReimen <kei.nakata.job@gmail.com>
2026-07-05 16:44:13 +0900

    [update] Add DailyTask

---
+++ domain/src/daily_task.rs
@@ -0,0 +1,51 @@
+use crate::task::{TaskPriority, Task};
+use jiff::civil::Date;
+use anyhow::{Result, bail};
+
+pub struct DailyTask {
+    pub id: i32,
+    pub project: String,
+    pub title: String,
+    pub detail: String,
+    pub priority: TaskPriority,
+}
+
+impl Default for DailyTask {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            project: String::new(),
+            title: String::new(),
+            detail: String::new(),
+            priority: TaskPriority::Low,
+        }
+    }
+}
+
+impl DailyTask {
+    pub fn is_valid(&self) -> bool {
+        !self.project.trim().is_empty() && !self.title.trim().is_empty()
+    }
+
+    pub fn create_task(&self, today: &Date) -> Result<Task> {
+        if !self.is_valid() {
+            bail!("Invalid daily task.");
+        }
+
+        let title_with_date = format!("{} for {}", self.title, today.strftime("%Y/%m/%d"));
+
+        let task_target_date = *today;
+
+        Ok(Task {
+            id: self.id,
+            project: self.project.clone(),
+            title: title_with_date,
+            detail: self.detail.clone(),
+            priority: self.priority,
+            start_date: task_target_date,
+            due_date: task_target_date,
+            entry_date: task_target_date,
+            ..Task::default()
+        })
+    }
+}
--- domain/src/lib.rs
+++ domain/src/lib.rs
@@ -1,3 +1,6 @@
 mod task;
 pub use task::{Task, TaskPriority, TaskStatus};
 pub use task::{TaskFilterFlags, TaskOrderFlags, TaskSearchFlags};
+
+mod daily_task;
+pub use daily_task::DailyTask;
