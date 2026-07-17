7920e3ec03528b0d945dd639bf86fe3b80d80213
MoriokaReimen <menschenjager.mark.neun@gmail.com>
2026-07-18 04:24:20 +0900

    [update] update chocolatey scripts

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageName = 'task-fighter'
 $toolsDir    = $PSScriptRoot
 $url32       = 'https://github.com/MoriokaReimen/Task-Fighter/releases/download/8.3.0/task-fighter-8.3.0-i686-pc-windows-msvc.zip'
 $url64       = 'https://github.com/MoriokaReimen/Task-Fighter/releases/download/8.3.0/task-fighter-8.3.0-x86_64-pc-windows-msvc.zip'
-$checksum32  = 'F620F6269E40146F3C2F9BADEC7985C4C88D9AA8E0AE903DCEADD822BB8F3FA2'
-$checksum64  = '9EAE318C30974864F80ED8F4CA65CC0689597E0A3430623A27B7C8E64848BD5A'
+$checksum32  = '47939D9D0C3CB9CFE2514916EBD11C8796787EF489D35CA9BA1C5C2147403A71'
+$checksum64  = '71B65395735D4687009CA9DC75D1D255A526FC7944C085CCFD72F014CEBD113F'
 
 $packageArgs = @{
   packageName    = $packageName
